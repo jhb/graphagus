e123986611fa47ee7c59c31470bdc747109c005a
Joerg Baach <mail@baach.de>
2014-10-04 14:51:02 +0200

    documentation

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from setuptools import setup, find_packages
 import sys, os
 
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 version = '0.1'
 
 setup(name='graphagus',
       version=version,
-      description="A graph database on top of ZODB",
-      long_description="""\
-""",
+      description="A graph database for property graphs on top of ZODB",
+      long_description="""Please read https://rawgit.com/jhb/graphagus/master/graphagus/README.html""",
       classifiers=["License :: OSI Approved :: GNU General Public License (GPL)",
           "Programming Language :: Python",
           "Development Status :: 3 - Alpha",
@@ -23,6 +25,10 @@ setup(name='graphagus',
       zip_safe=False,
       install_requires=[
           # -*- Extra requirements: -*-
+          'ZODB',
+          'ZEO',
+          'repoze.catalog',
+          'graphviz'
       ],
       entry_points="""
       # -*- Entry points: -*-
