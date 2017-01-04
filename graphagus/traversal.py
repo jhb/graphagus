8deddc5ca90f44b2780174d4aeb5e1d30d0e178d
Joerg Baach <mail@baach.de>
2017-01-04 18:50:01 +0100

    methods for incoming connections

--- graphagus/traversal.py
+++ graphagus/traversal.py
@@ -16,7 +16,7 @@ class Traverser(object):
     def __iter__(self):
         return self.input
 
-    def x__repr__(self):
+    def __repr__(self):
         return repr(list(self.input))
 
     def t(self,data,store):
@@ -42,8 +42,11 @@ class Traverser(object):
 
     def values(self,key):
         s = self.storage()
-        for i in self.input:
-            yield(s[i][key])
+
+        def gendata():
+            for i in self.input:
+                yield(s[i][key])
+        return self.t(gendata(),self.store)
 
     def outE(self,types=None):
         data = self.g.getAllEdges(self.input,'o',types,1)
@@ -56,6 +59,17 @@ class Traverser(object):
     def inN(self):
         return self.t([self.g.edges[i][1] for i in self.input],'nodes')
 
+    def inE(self,types=None):
+        data = self.g.getAllEdges(self.input,'i',types,1)
+        return self.t(data,'edges')
+
+    def _in(self,types=None):
+        data = [e[0] for e in self.g.getAllEdges(self.input, 'i', types, 0)]
+        return self.t(data,'nodes')
+
+    def outN(self):
+        return self.t([self.g.edges[i][0] for i in self.input],'nodes')
+
     def has(self,**kwargs):
         s = self.storage()
 
@@ -64,8 +78,12 @@ class Traverser(object):
                 bad = 0;
                 props = s[i]
                 for key,cmp in kwargs.items():
-                    if not cmp(props[key]):
-                        bad +=1
+                    if callable(cmp):
+                        if not cmp(props[key]):
+                            bad +=1
+                    else:
+                        if not props[key] == cmp:
+                            bad +=1
                 if not bad:
                     yield i
 
@@ -74,6 +92,8 @@ class Traverser(object):
 
     def gt(self,value):
         return lambda x: x > value
+
+T = Traverser
 
 def getTraverser(graph):
     return Traverser(graph)
